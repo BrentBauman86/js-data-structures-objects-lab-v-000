1117e091f3decd9449702c9bd405000b1fbf996e
Brent Bauman <brentbauman86@gmail.com>
2018-11-22 14:55:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // Write your solution in this file!
 const driver = {
-  driver: "bob"
 };
 
 function updateDriverWithKeyAndValue() {
-  driver: "tim"
 };
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
